0145ed352d1bc3273feb4d88910b52f7d4a99f64
Ashylila <ashy.suhr@gmail.com>
2025-05-03 14:56:29 +0200

    Initial commit

---
+++ TheCollector/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+
+namespace TheCollector;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 0;
+    
+
+    // the below exist just to make saving less cumbersome
+    public void Save()
+    {
+        Plugin.PluginInterface.SavePluginConfig(this);
+    }
+}
---
+++ TheCollector/Windows/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using Lumina.Excel.Sheets;
+
+namespace TheCollector.Windows;
+
+public class MainWindow : Window, IDisposable
+{
+    private string GoatImagePath;
+    private Plugin Plugin;
+
+    // We give this window a hidden ID using ##
+    // So that the user will see "My Amazing Window" as window title,
+    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
+    public MainWindow(Plugin plugin)
+        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+        
+        Plugin = plugin;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+
+
+        
+    }
+}
